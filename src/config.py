b2d9013bbc767d1ebd02d40ac80903961f13a361
mingen <mingen.yeo.2023@techpass.gov.sg>
2025-11-05 17:34:10 +0800

    service url naming

--- src/config.py
+++ src/config.py
@@ -2,11 +2,11 @@ import os
 
 
 # Service base URLs
-ADMIN_SERVICE_URL = os.getenv("ADMIN_SERVICE_URL", "http://localhost:5001")
-BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://localhost:5000")
-PAYMENT_SERVICE_URL = os.getenv("PAYMENT_SERVICE_URL", "http://localhost:5002")
-NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL", "http://localhost:5004")
+ADMIN_SERVICE_URL = os.getenv("ADMIN_SERVICE_URL", "http://admin.tickets.local:8081")
+BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://ticket-booking.tickets.local:8084")
+PAYMENT_SERVICE_URL = os.getenv("PAYMENT_SERVICE_URL", "http://payment.tickets.local:8083")
+NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL", "http://notification.tickets.local:8082")
 
-# Networking and resiliencya
+# Networking and resiliency
 REQUEST_TIMEOUT = float(os.getenv("REQUEST_TIMEOUT_MS", "5000")) / 1000.0
 RETRY_MAX_ATTEMPTS = int(os.getenv("RETRY_MAX_ATTEMPTS", "3"))
