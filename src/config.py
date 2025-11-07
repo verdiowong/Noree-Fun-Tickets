de76978d672791eec625153d8fa7b0e09c01a9ab
mingen <mingen.yeo.2023@techpass.gov.sg>
2025-11-07 17:23:33 +0800

    change payment and notificaiton endpoint

--- src/config.py
+++ src/config.py
@@ -5,8 +5,8 @@ import os
 ADMIN_SERVICE_URL = os.getenv("ADMIN_SERVICE_URL", "http://admin.tickets.local:8081")
 BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://ticket-booking-service.tickets.local:8084")
 # BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://10.0.11.26:8084")
-PAYMENT_SERVICE_URL = os.getenv("PAYMENT_SERVICE_URL", "http://payment.tickets.local:8083")
-NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL", "http://notification.tickets.local:8082")
+PAYMENT_SERVICE_URL = os.getenv("PAYMENT_SERVICE_URL", "http://payments.tickets.local:8083")
+NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL", "http://notifications.tickets.local:8082")
 
 # Networking and resiliency
 REQUEST_TIMEOUT = float(os.getenv("REQUEST_TIMEOUT_MS", "5000")) / 1000.0
