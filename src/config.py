669c30822b180a6a60d789012ed197f97b666015
mingen <mingen.yeo.2023@techpass.gov.sg>
2025-11-07 16:45:30 +0800

    ticket bookign endpoint

--- src/config.py
+++ src/config.py
@@ -3,8 +3,8 @@ import os
 
 # Service base URLs
 ADMIN_SERVICE_URL = os.getenv("ADMIN_SERVICE_URL", "http://admin.tickets.local:8081")
-# BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://ticket-booking.tickets.local:8084")
-BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://10.0.11.26:8084")
+BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://ticket-booking-service.tickets.local:8084")
+# BOOKING_SERVICE_URL = os.getenv("BOOKING_SERVICE_URL", "http://10.0.11.26:8084")
 PAYMENT_SERVICE_URL = os.getenv("PAYMENT_SERVICE_URL", "http://payment.tickets.local:8083")
 NOTIFICATION_SERVICE_URL = os.getenv("NOTIFICATION_SERVICE_URL", "http://notification.tickets.local:8082")
 
